52e12e354702dfbe6f684ff86ef89570a2ffc0fb
Bright238 <brightmafungautsikapamulomo@gmail.com>
2024-11-22 14:58:31 +0200

    improve_dashboard_button_visual

--- src/pages/DashboardPages/NftDashboardPage.tsx
+++ src/pages/DashboardPages/NftDashboardPage.tsx
@@ -3,14 +3,13 @@ import { PageTitle } from '@app/components/common/PageTitle/PageTitle';
 import { References } from '@app/components/common/References/References';
 import { useResponsive } from '@app/hooks/useResponsive';
 import { Balance } from '@app/components/nft-dashboard/Balance/Balance';
-import * as S from './DashboardPage.styles';
 import { BaseRow } from '@app/components/common/BaseRow/BaseRow';
 import { BaseCol } from '@app/components/common/BaseCol/BaseCol';
 import { VisitorsPieChart } from '@app/components/charts/VisitorsPieChart';
-import { Button, Skeleton, Spin, Typography } from 'antd';
+import { Button, Skeleton, Typography } from 'antd';
 import axios from 'axios';
 import { GradientStackedAreaChart } from '@app/components/charts/GradientStackedAreaChart/GradientStackedAreaChart';
-import { DashboardOutlined } from '@ant-design/icons';
+import { RightOutlined, RiseOutlined } from '@ant-design/icons';
 import { useNavigate } from 'react-router-dom';
 
 interface User {
@@ -28,7 +27,6 @@ const MedicalDashboardPage: React.FC = () => {
   const [user, setUser] = useState<User | null>(null);
   const [vcasCount, setVCAsCount] = useState(0);
   const [householdCount, setHouseholdCount] = useState(0);
-  const [membersCount, setMembersCount] = useState(0);
   const [districtLoading, setDistrictLoading] = useState(true);
 
   useEffect(() => {
@@ -92,26 +90,25 @@ const MedicalDashboardPage: React.FC = () => {
   const desktopLayout = (
     <>
       <div style={{ margin: '20px' }}>
-        <br />
-        <BaseRow gutter={[80, 80]} justify="end">
+        <BaseRow justify="end" align="middle" style={{ marginBottom: '20px' }}>
           <BaseCol>
-            <Button
-              type="dashed"
-              style={{ borderRadius: '100px' }}
-              icon={<DashboardOutlined />}
-              size="large"
-              onClick={handleViewDashboards} // Use the handle function
-            >
-              View Dashboards
-            </Button>
-            <br />
-            <br />
+          <Button
+          style={{width: '100%', borderTopRightRadius: '50%'}}
+             type="ghost"
+            onClick={handleViewDashboards}
+          >
+            View Dashboards <RiseOutlined />
+          </Button>
           </BaseCol>
         </BaseRow>
         <BaseRow gutter={[80, 80]}>
           <BaseCol span={6}>
-            <Typography.Title style={{ textAlign: "center" }} level={4}>Location</Typography.Title>
-            <Typography.Title style={{ textAlign: "center" }} level={4}>   {districtLoading ? <Skeleton /> : `${user?.location}`} District</Typography.Title>
+            <Typography.Title style={{ textAlign: 'center' }} level={4}>
+              Location
+            </Typography.Title>
+            <Typography.Title style={{ textAlign: 'center' }} level={4}>
+              {districtLoading ? <Skeleton /> : `${user?.location} District`}
+            </Typography.Title>
           </BaseCol>
           <BaseCol xs={24} lg={9}>
             <div id="balance" style={{ marginBottom: '20px' }}>
@@ -125,61 +122,50 @@ const MedicalDashboardPage: React.FC = () => {
           </BaseCol>
         </BaseRow>
         <BaseRow gutter={[60, 60]} style={{ marginTop: '40px' }}>
-          <BaseCol xs={24} lg={12}>
-            <VisitorsPieChart />
-          </BaseCol>
-          <BaseCol xs={24} lg={12}>
+          <BaseCol xs={24} xxl={12}>
             <GradientStackedAreaChart />
           </BaseCol>
         </BaseRow>
         <References />
       </div>
     </>
-
   );
 
   const mobileAndTabletLayout = (
     <>
+      <BaseRow gutter={[20, 24]} style={{ marginBottom: '20px', textAlign: 'center' }}>
+      </BaseRow>
       <BaseRow gutter={[20, 24]}>
-        <BaseRow gutter={[60, 60]}>
-          <BaseCol xs={24} sm={12} md={8} lg={6} xl={6}>
-            <br />
-            <br />
-            <BaseCol>
-              <Typography.Title style={{ textAlign: "center" }} level={4}>Location</Typography.Title>
-              <Typography.Title style={{ textAlign: "center" }} level={4}>   {districtLoading ? <Skeleton /> : `${user?.location}`} District</Typography.Title>
-            </BaseCol>
-          </BaseCol>
-          <BaseCol xs={24} sm={12} md={8} lg={6} xl={6} style={{ textAlign: 'center' }}>
-            <Button
-              type="dashed"
-              style={{ borderRadius: '100px' }}
-              icon={<DashboardOutlined />}
-              size="large"
-              onClick={handleViewDashboards}
-            >
-              View Dashboards
-            </Button>
-          </BaseCol>
-          <BaseCol xs={24} sm={12} md={8} lg={6} xl={6}>
-            <div id="balance">
-              <Balance title="Households" count={householdCount} />
-            </div>
-          </BaseCol>
-          <BaseCol xs={24} sm={12} md={8} lg={6} xl={6}>
-            <div id="balance">
-              <Balance title="VCAs" count={vcasCount} />
-            </div>
-          </BaseCol>
-        </BaseRow>
-        <BaseRow gutter={[60, 60]} style={{ marginTop: '40px' }}>
-          <BaseCol xs={24} lg={12}>
-            <VisitorsPieChart />
-          </BaseCol>
-          <BaseCol xs={24} lg={12}>
-            <GradientStackedAreaChart />
-          </BaseCol>
-        </BaseRow>
+        <BaseCol xs={24} sm={12} md={8}>
+          <Typography.Title style={{ textAlign: 'center' }} level={4}>
+            Location
+          </Typography.Title>
+          <Typography.Title style={{ textAlign: 'center' }} level={4}>
+            {districtLoading ? <Skeleton /> : `${user?.location} District`}
+          </Typography.Title>
+        </BaseCol>
+        <BaseCol xs={24} sm={12} md={8}>
+          <div id="balance">
+            <Balance title="Households" count={householdCount} />
+          </div>
+        </BaseCol>
+        <BaseCol xs={24} sm={12} md={8}>
+          <div id="balance">
+            <Balance title="VCAs" count={vcasCount} />
+          </div>
+        </BaseCol>
+        <BaseCol span={24}>
+          <Button
+          style={{width: '100%', borderTopRightRadius: '50%'}}
+            type="primary"
+            onClick={handleViewDashboards}
+          >
+            View Dashboards <RiseOutlined />
+          </Button>
+        </BaseCol>
+        <BaseCol xs={24} lg={12}>
+          <GradientStackedAreaChart />
+        </BaseCol>
       </BaseRow>
     </>
   );
